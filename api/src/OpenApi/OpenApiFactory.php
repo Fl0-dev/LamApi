f5048e202e8967e2bb7d143c4f5d009123f6a2a1
Fl0-dev <florian@lamacompta.co>
2022-08-04 10:11:05 +0200

    Add method for remove parameters for openapi

--- api/src/OpenApi/OpenApiFactory.php
+++ api/src/OpenApi/OpenApiFactory.php
@@ -3,6 +3,7 @@
 namespace App\OpenApi;
 
 use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\Model\PathItem;
 use ApiPlatform\Core\OpenApi\OpenApi;
 
 class OpenApiFactory implements OpenApiFactoryInterface
@@ -20,7 +21,29 @@ class OpenApiFactory implements OpenApiFactoryInterface
             if ($pathItem->getGet() && $pathItem->getGet()->getSummary() === 'hidden') {//on récupère les paths dont le summary est 'hidden'
                 $openApi->getPaths()->addPath($path, $pathItem->withGet(null));//on supprime le get
             }
+            
+            // remove identifier parameter from operations which include "#withoutIdentifier" in the description
+            foreach (PathItem::$methods as $method) {
+                $getter = 'get'.ucfirst(strtolower($method));
+                $setter = 'with'.ucfirst(strtolower($method));
+                /** @var Operation $operation */
+                $operation = $pathItem->$getter();
+                if ($operation && preg_match('/#withoutIdentifier/', $operation->getDescription())) {
+                    /** @var Parameter[] $parameters */
+                    $parameters = $operation->getParameters();
+                    foreach ($parameters as $i => $parameter) {
+                        if (preg_match('/identifier/i', $parameter->getDescription())) {
+                            unset($parameters[$i]);
+                            break;
+                        }
+                    }
+                    $openApi->getPaths()->addPath($path, $pathItem = $pathItem->$setter($operation->withParameters(array_values($parameters))));
+                }
+            }
         }
+
+
+
 
         return $openApi;
     }
