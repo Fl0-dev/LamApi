56e0eb0b312bd84f3e933a02d220b4ff16df0f30
Fl0-dev <florian@lamacompta.co>
2022-09-13 14:25:29 +0200

    #30-add DataFixtures

--- api/src/DataFixtures/OfferResearchFixtures.php
+++ api/src/DataFixtures/OfferResearchFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\References\Experience;
 use App\Entity\Research\OfferResearch;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -12,9 +13,23 @@ class OfferResearchFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         $offerResearch = new OfferResearch();
+        $offerResearch->setCreatedDate(new \Datetime());
         $offerResearch->addCity($this->getReference(CityFixtures::CITY_REFERENCE_1));
-        
-        
+        $offerResearch->addJobTitle($this->getReference(JobTitleFixtures::JOB_TITLE_REFERENCE_1));
+        $offerResearch->addDepartment($this->getReference(DepartmentFixtures::DEPARTMENT_REFERENCE_1));
+        $offerResearch->setNbResult(10);
+
+        $manager->persist($offerResearch);
+        $manager->flush();
+
+        $offerResearch = new OfferResearch();
+        $offerResearch->setCreatedDate(new \Datetime());
+        $offerResearch->addCity($this->getReference(CityFixtures::CITY_REFERENCE_2));
+        $offerResearch->addJobTitle($this->getReference(JobTitleFixtures::JOB_TITLE_REFERENCE_2));
+        $offerResearch->addExperience((new Experience(Experience::SENIOR, 'Lamasenior', 'Lamasenior (2 à 5 ans)', "de 2 à 5 ans d'expérience",24))->getId());
+        $offerResearch->addExperience((new Experience(Experience::JUNIOR, 'Lamajunior', 'Lamajunior (- 1 an)', "< 1 an d'expérience",0))->getId());
+        $offerResearch->setNbResult(10);
+
         $manager->persist($offerResearch);
         $manager->flush();
     }
@@ -23,7 +38,8 @@ class OfferResearchFixtures extends Fixture implements DependentFixtureInterface
     {
         return [
             CityFixtures::class,
-            JobTypeFixtures::class,
+            DepartmentFixtures::class,
+            JobTitleFixtures::class,
             ToolFixtures::class,
             BadgeFixtures::class,
         ];
