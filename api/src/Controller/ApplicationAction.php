2cfc40c7701d150ef8c5e3dbd51959b3606160b7
Fl0-dev <florian@lamacompta.co>
2022-10-12 15:04:06 +0200

    #64-Changes in .gitlab-ci + fix phpcs warnings

--- api/src/Controller/ApplicationAction.php
+++ api/src/Controller/ApplicationAction.php
@@ -19,8 +19,8 @@ class ApplicationAction extends AbstractController
     public const APPLICATION_PROPERTY_FILE = 'file';
     public const POST_APPLICATION_SPONTANEOUS_IDENTIFIER_NAME = 'companyEntityOfficeId';
     public const POST_APPLICATION_OFFER_IDENTIFIER_NAME = 'offerId';
-    public const ENDPOINT_FOR_POST_APPLICATION_BY_OFFER_ID = '_api_/applications/{offerId}_post';
-    public const ENDPOINT_FOR_POST_APPLICATION_SPONTANEOUS = '_api_/applications/spontaneous/{companyEntityOfficeId}_post';
+    public const ENDPOINT_POST_APPLICATION_BY_OFFER_ID = '_api_/applications/{offerId}_post';
+    public const ENDPOINT_POST_APPLICATION_SPONTANEOUS = '_api_/applications/spontaneous/{companyEntityOfficeId}_post';
 
     public function __construct(
         private CompanyEntityOfficeRepository $companyEntityOfficeRepository,
@@ -32,7 +32,7 @@ class ApplicationAction extends AbstractController
     {
         $endpoint = $request->attributes->get('_route');
 
-        if ($endpoint === self::ENDPOINT_FOR_POST_APPLICATION_SPONTANEOUS) {
+        if ($endpoint === self::ENDPOINT_POST_APPLICATION_SPONTANEOUS) {
             $companyEntityOfficeId = $request->attributes->get(self::POST_APPLICATION_SPONTANEOUS_IDENTIFIER_NAME);
             $file = $request->files->get(self::APPLICATION_PROPERTY_FILE);
 
@@ -64,7 +64,7 @@ class ApplicationAction extends AbstractController
             return $application;
         }
 
-        if ($endpoint === self::ENDPOINT_FOR_POST_APPLICATION_BY_OFFER_ID) {
+        if ($endpoint === self::ENDPOINT_POST_APPLICATION_BY_OFFER_ID) {
             $offerId = $request->attributes->get(self::POST_APPLICATION_OFFER_IDENTIFIER_NAME);
             $file = $request->files->get(self::APPLICATION_PROPERTY_FILE);
 
