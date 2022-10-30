2b95055f2a3450b622058031148fc8b8f1f5f506
Fl0-dev <florian@lamacompta.co>
2022-10-30 14:49:21 +0100

    #41-Add results entities for Lamatch Subscription

---
+++ api/src/Repository/Applicant/Subscriptions/ApplicantLamatchRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository\Applicant\Subscriptions;
+
+use App\Entity\Applicant\Subscriptions\ApplicantLamatch;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ApplicantLamatch>
+ *
+ * @method ApplicantLamatch|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ApplicantLamatch|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ApplicantLamatch[]    findAll()
+ * @method ApplicantLamatch[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ApplicantLamatchRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ApplicantLamatch::class);
+    }
+
+    public function save(ApplicantLamatch $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(ApplicantLamatch $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return ApplicantLamatch[] Returns an array of ApplicantLamatch objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?ApplicantLamatch
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
---
+++ api/src/Repository/Applicant/Subscriptions/CompanyGroupResultRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository\Applicant\Subscriptions;
+
+use App\Entity\Applicant\Subscriptions\CompanyGroupResult;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<CompanyGroupResult>
+ *
+ * @method CompanyGroupResult|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CompanyGroupResult|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CompanyGroupResult[]    findAll()
+ * @method CompanyGroupResult[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CompanyGroupResultRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CompanyGroupResult::class);
+    }
+
+    public function save(CompanyGroupResult $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(CompanyGroupResult $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return CompanyGroupResult[] Returns an array of CompanyGroupResult objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?CompanyGroupResult
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
