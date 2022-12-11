d3c47c255540746fb85cadd0e9daeecdb37873ee
Fl0-dev <florian@lamacompta.co>
2022-12-11 09:39:45 +0100

    #83-Add verification in controller + Fix endpoint for get one ApplicantResult + add TODOs for factorization

--- api/src/Controller/PostEmployerFavoriteApplicantResult.php
+++ api/src/Controller/PostEmployerFavoriteApplicantResult.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use App\Entity\Subscriptions\Employer\Lamatch\ApplicantResult;
 use App\Entity\Subscriptions\Employer\Lamatch\EmployerFavoriteCandidat;
 use App\Entity\User\Employer;
+use App\Repository\SubscriptionRepositories\Employer\EmployerFavoriteCandidatRepository;
+use App\Repository\SubscriptionRepositories\Employer\EmployerSubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 class PostEmployerFavoriteApplicantResult extends AbstractController
 {
+    public function __construct(
+        private EmployerSubscriptionRepository $employerSubscriptionRepository,
+        private EmployerFavoriteCandidatRepository $employerFavoriteCandidatRepository,
+    ) {
+    }
+
     public function __invoke(Request $request): ?EmployerFavoriteCandidat
     {
         $employer = $this->getUser();
+
         if (!$employer instanceof Employer) {
             throw new \Exception('Employer not found');
         }
@@ -22,6 +31,33 @@ class PostEmployerFavoriteApplicantResult extends AbstractController
 
         if (!$applicantResult instanceof ApplicantResult) {
             throw new \Exception('ApplicantResult not found');
+        }
+        //TODO: factorize this code in a service
+        $employerLamatchSubscriptionId = $applicantResult->getEmployerLamatch()
+            ->getEmployerLamatchSubscription()->getId();
+
+        $currentEmployerSubscription = $this->employerSubscriptionRepository->findOneBy([
+            'employer' => $employer,
+        ]);
+
+        if (!$currentEmployerSubscription) {
+            throw new \Exception('Employer subscription not found');
+        }
+
+        if (
+            $employerLamatchSubscriptionId
+            !== $currentEmployerSubscription->getLamatchSubscription()->getId()
+        ) {
+            throw new \Exception('ApplicantResult denied');
+        }
+
+        $ResearchEmployerFavoriteCandidat = $this->employerFavoriteCandidatRepository->findOneBy([
+            'applicantResult' => $applicantResult,
+            'employer' => $employer,
+        ]);
+
+        if ($ResearchEmployerFavoriteCandidat) {
+            throw new \Exception('ApplicantResult already in favorite');
         }
 
         $employerFavoriteCandidat->setApplicantResult($applicantResult);
